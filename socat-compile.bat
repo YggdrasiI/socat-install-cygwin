edba5d1561566f3f77598d7e650987f53b572da5
Olaf Schulz <947069+YggdrasiI@users.noreply.github.com>
2023-03-15 11:17:52 +0100

    Waiting on cygwin-setup and merging bash script calls in new script

--- socat-compile.bat
+++ socat-compile.bat
@@ -7,7 +7,8 @@ REM --- https://github.com/valorisa/socat-1.7.4.4_for_Windows
  
 SETLOCAL
 
-SET TEMPDIR=C:\Temp
+SET SCRIPTDIR=%CD%
+SET TEMPDIR=I:\Tmp
 
 REM -- change to C:\Temp 
 if NOT EXIST %TEMPDIR% (
@@ -26,28 +27,51 @@ IF NOT EXIST cygwin-setup64.exe (
 REM -- Configure our paths
 SET SITE=http://ucmirror.canterbury.ac.nz/cygwin
 SET LOCALDIR=%CD%
-SET ROOTDIR=C:\cygwin64
+SET ROOTDIR=I:\Cygwin\cygwin64
 SET CYGWINROOTDIR=\
 SET SOCAT=socat-1.7.4.4
+SET SOCAT_REQUIRED_CYGWIN_RUNTIME_LIBS=crypto-1.1 ncursesw-10 readline7 ssl-1.1 win1 wrap-0 z
+
  
 REM -- These are the packages we will install to compile socat (in addition to the default packages)
 SET PACKAGES=wget,gcc-g++,gcc-core,make,gcc-fortran,gcc-objc,gcc-objc++,libkrb5-devel,libkrb5_3,libreadline-devel,libssl-devel,libwrap-devel,tcp_wrappers
  
-REM -- More info on command line options at: https://cygwin.com/faq/faq.html#faq.setup.cli
-REM -- Do it!
-ECHO *** INSTALLING DEFAULT PACKAGES
-cygwin-setup64 --quiet-mode --no-desktop --download --local-install --no-verify -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%"
-ECHO.
-ECHO.
-ECHO *** INSTALLING CUSTOM PACKAGES
-cygwin-setup64 -q -d -D -L -X -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %PACKAGES%
+
+IF NOT EXIST "%ROOTDIR%\bin\bash.exe" (
+	REM -- More info on command line options at: https://cygwin.com/faq/faq.html#faq.setup.cli
+	REM -- Do it!
+	ECHO *** INSTALLING DEFAULT PACKAGES
+	call cygwin-setup64 --quiet-mode --no-desktop --download --local-install --no-verify -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%"
+	ECHO.
+	ECHO Please wait with continuation until cygwin setup completes.
+	pause
+) ELSE (
+	ECHO Cygwin default packages already installed.
+)
+
+REM %ROOTDIR%\bin\cygcheck.exe -c can be used to compare our package
+REM list with the installed list. We just check for one file to keep it simple...
+IF NOT EXIST "%ROOTDIR%\lib\libwrap.dll.a" (
+
+	ECHO.
+	ECHO *** INSTALLING CUSTOM PACKAGES
+	call cygwin-setup64 -q -d -D -L -X -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %PACKAGES%
+
+	REM -- Show what we did
+	ECHO.
+	ECHO.
+	ECHO cygwin installation updated
+	ECHO  - %PACKAGES%
+	ECHO.
+	ECHO.
+	ECHO Please wait with continuation until cygwin setup completes.
+	pause
+) ELSE (
+	ECHO Cygwin custom packages already installed.
+	ECHO Skipping cygwin-setup64 call.
+)
  
-REM -- Show what we did
-ECHO.
-ECHO.
-ECHO cygwin installation updated
-ECHO  - %PACKAGES%
-ECHO.
+
 
 REM -- Change to Cygwin Root folder
 CD %ROOTDIR%
@@ -55,22 +79,16 @@ CD %ROOTDIR%
 REM -- Download socat
 IF NOT EXIST %SOCAT:~0%.tar.gz (
 	ECHO %SOCAT:~0%.tar.gz NOT found! Downloading Socat...
-	bitsadmin /transfer socatDownloadJob /download /priority normal http://www.dest-unreach.org/socat/download/%SOCAT:~0%.tar.gz %CD%\%SOCAT:~0%.tar.gz
+	bitsadmin /transfer socatDownloadJob /download /priority normal "http://www.dest-unreach.org/socat/download/%SOCAT:~0%.tar.gz" "%CD%\%SOCAT:~0%.tar.gz"
 ) ELSE (
 	ECHO %SOCAT:~0%.tar.gz found! Skipping socat download...
 )
 
-REM -- Extract Socat
-%ROOTDIR%\bin\bash.exe --login -c "cd / && tar xzf socat*.tar.gz"
+REM Proceed in bash script
+cd %SCRIPTDIR%
+copy "%SCRIPTDIR%\socat-compile.sh" "%ROOTDIR%\."
+%ROOTDIR%\bin\bash.exe --login -c "cd / && ls && ./socat-compile.sh"
 
-REM -- Configure Socat
-%ROOTDIR%\bin\bash.exe --login -c "cd $(find / -maxdepth 1 -type d -name 'socat*') && ./configure"
-
-REM -- Make Socat
-%ROOTDIR%\bin\bash.exe --login -c "cd $(find / -maxdepth 1 -type d -name 'socat*') && make"
-
-REM -- Copy cygwin .dll's into socat folder
-%ROOTDIR%\bin\bash.exe --login -c "find /bin/ -name 'cyg*.dll' -exec cp {} $(find / -maxdepth 1 -type d -name 'socat*') \;"
 
 ECHO.
 ECHO Socat is compiled and installed! You should see an error that socat expected two inputs but none were given, below.
@@ -78,7 +96,7 @@ ECHO.
 ECHO.
 
 REM -- test Socat
-%ROOTDIR%\%SOCAT%\socat.exe
+%ROOTDIR%\%SOCAT%\release\socat.exe
 
 PAUSE
 EXIT /B 0
---
+++ socat-compile.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Contiuation of socat-compile.bat
+
+SOCAT_TAR="$(find / -maxdepth 1 -type f -name 'socat*.tar.gz' | tail -n 1)"
+SOCAT_DIR="${SOCAT_TAR%.tar.gz}"
+
+# If older version installed and tar.gz was deleted use existing variant
+OLDER_SOCAT_DIR="$(find / -maxdepth 1 -type d -name 'socat*' | tail -n 1)"
+test -z "${SOCAT_DIR}" && SOCAT_DIR="${OLDER_SOCAT_DIR}"
+
+SOCAT_REQUIRED_CYGWIN_RUNTIME_LIBS="crypto-1.1 ncursesw-10 readline7 ssl-1.1 win1 wrap-0 z"
+
+# Extracting source files
+echo "Tar file: $SOCAT_TAR"
+if [ ! -f "$SOCAT_TAR" -a ! -d "$SOCAT_DIR" ] ; then
+	echo "Can not find sofar tar.gz-file!"
+	exit -1
+fi
+
+if [ -z "$SOCAT_DIR" -o ! -d "$SOCAT_DIR" ] ; then
+	echo "Extract tar file"
+	tar -xzf "$SOCAT_TAR"
+	SOCAT_DIR="$(find / -maxdepth 1 -type d -name 'socat*' | tail -n 1)"
+fi
+
+echo "Source folder: $SOCAT_DIR"
+if [ ! -d "$SOCAT_DIR" ] ; then
+	echo "Can not find socat source folder"
+	exit -1
+fi
+
+cd "$SOCAT_DIR"
+
+# Configure
+if [ ! -f Makefile ] ; then
+	./configure
+fi
+
+# Compiling
+echo "Build socat"
+make
+
+# Export required files
+echo "Build release"
+mkdir -p release
+cp -u socat.exe release/.
+for LIBNAME in $SOCAT_REQUIRED_CYGWIN_RUNTIME_LIBS ; do 
+	# echo "$LIBNAME"
+	cp -u "/bin/cyg${LIBNAME}.dll" "release/."
+done
